338fc0434b0383422d52bdf90cc3451173ddb9f2
Aniela Parobek <ellie.parobek@gmail.com>
2019-10-15 14:16:25 -0400

    Initial commit

---
+++ AbsoluteValueTest/AbsoluteValueTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace AbsoluteValueTest
+{
+    public class AbsoluteValueTest
+    {
+        public static void Main(string[] args)
+        {
+            string key = "r";
+            do
+            {
+                var absolute = 0;
+                Stopwatch stopwatch = new Stopwatch();
+                List<TimeSpan> values = new List<TimeSpan>();
+
+                // Aboslute Value time
+                for (int j = 0; j < 1000; j++)
+                {
+                    stopwatch.Start();
+                    for (int i = -1; i >= -1000000; i--)
+                    {
+                        absolute = Math.Abs(i);
+                    }
+                    stopwatch.Stop();
+                    values.Add(stopwatch.Elapsed);
+                    stopwatch.Reset();
+                }
+
+                stopwatch.Stop();
+                Console.WriteLine("Absolute Value average time (in ms): " + values.Average(timeSpan => timeSpan.Milliseconds));
+
+                // Multiplied by -1 time
+                values.Clear();
+                for (int j = 0; j < 1000; j++)
+                {
+                    stopwatch.Start();
+                    for (int i = -1; i >= -1000000; i--)
+                    {
+                        absolute = i * -1;
+                    }
+                    stopwatch.Stop();
+                    values.Add(stopwatch.Elapsed);
+                    stopwatch.Reset();
+                }
+
+                stopwatch.Stop();
+                Console.WriteLine("Multiplied by -1 average time (in ms): " + values.Average(timeSpan => timeSpan.Milliseconds));
+
+                // Multiplied by -1 with negative check time
+                values.Clear();
+                for (int j = 0; j < 1000; j++)
+                {
+                    stopwatch.Start();
+                    for (int i = -1; i >= -1000000; i--)
+                    {
+                        if (i < 0)
+                        {
+                            absolute = i * -1;
+                        }
+                    }
+                    stopwatch.Stop();
+                    values.Add(stopwatch.Elapsed);
+                    stopwatch.Reset();
+                }
+
+                stopwatch.Stop();
+                Console.WriteLine("Multiplied by -1 with negative check average time (in ms): " + values.Average(timeSpan => timeSpan.Milliseconds));
+                Console.WriteLine("Type 'r' to run again, any other key to exit.");
+                key = Console.ReadLine();
+            } while (key.Equals("r"));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+** Absolute Value Test **
+A program to test the speeds of various methods to get the absolute value of a number in C#.
+
+** Results **
+The results were quite conclusive. I performed 3 tests:
+
+1. Math.Abs
+   
+```csharp
+absolute = Math.Abs(i);
+```
+
+2. Multiple by -1 without checking if the number is negative
+   
+```csharp
+absolute = i * -1;
+```
+
+3. Multiple by -1 while checking if the number is negative
+
+```csharp
+if (i < 0)
+{
+    absolute = i * -1;
+}
+```
+
+Each test took the abosulte value of a negative number looping from -1 to -1,000,000. The tests ran 1,000 times and the elapsed time to create each absolute value was averaged for the result. The results were consistently: Math.abs ~= 3.2ms, *-1 ~= 1ms, *-1 with negative check ~= 2ms.
+
+![Results](../master/media/absolutevaluetest.PNG)
+
+** Conclusion **
+If you know the number is negative, it seems to be best to multiple by negative one to get the absolute value. But in the end, it is such a miniscule difference and Math.abs is much more readable.
